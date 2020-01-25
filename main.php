f6fb8ceab7248dae4423568edc596161e546f1a0
gravl0 <andrej7aleksandrov@gmail.com>
2020-01-25 12:29:37 +0200

    New update

--- main.php
+++ main.php
@@ -1,5 +1,6 @@
 <?php
-
+$n = 112345678;
+session_start();
 require "vendor/autoload.php";
 
 $app = new \atk4\ui\App("andrej");
@@ -12,7 +13,11 @@ $col_1 = $columns->addColumn(3);
 $col_2 = $columns->addColumn(10);
 $col_3 = $columns->addColumn(3);
 $clikcer = $col_2->add(["Button","25","green fluid big"]);
-$clikcer->on('click',function($clikcer) { 
+$clikcer->on('click',function($clikcer) {
   $clikcer->set("Test");
   $clikcer->js()->reload();
 });
+
+$label = $col_2->add(["Button",$_SESSION['test']]);
+
+$save = $col_2->add(["Button","save","blue big"]);
--- reg.php
+++ reg.php
@@ -1,4 +1,8 @@
 <?php
+
+session_start();
+$_SESSION["test"] = "112345678";
+
 require "vendor/autoload.php";
 require "connection.php";
 $app = new \atk4\ui\App("andrej");
@@ -7,7 +11,12 @@ $app->initLayout("Centered");
 $form = $app->layout->add('Form');
 $form->setModel(new User($db));
 $form->buttonSave->set("Жми");
+$model = new User($db);
 
-$form->onSubmit(function($form) {
+$form->onSubmit(function($form) use($model) {
+  $nickname = $form->model['nickname'];
+  $form->model->save();
+  $model->tryLoadBy('nickname',$nickname);
+  $_SESSION["user_id"] = $model->id;
   return new \atk4\ui\jsExpression('document.location = "main.php"');
 });
