d4dd6cfcad022c8d1520e640bc141fa8ad91b12e
gravl0 <andrej7aleksandrov@gmail.com>
2020-02-01 12:32:50 +0200

    new update

--- index.php
+++ index.php
@@ -8,10 +8,10 @@ $app->initLayout("Centered");
 
 $clicker_name = $app->add(["Header","Colibri Clicker","centered"]);
 
-$button_new = $app->add(["Button","Start","circular icon blue","icon"=>"power off"]);
+$button_login = $app->add(["Button","Log in","circular icon blue","icon"=>"power off"]);
 
-$button_new->on('click', function() use($app) {
-  return new \atk4\ui\jsExpression('document.location = "main.php"');
+$button_login->on('click', function() use($app) {
+  return new \atk4\ui\jsExpression('document.location = "login.php"');
 });
 $button_reg = $app->add(["Button","Registration","green","icon"=>"arrow circle right"]);
 $button_reg->link('reg.php');
---
+++ login.php
@@ -0,0 +1,31 @@
+<?php
+
+require "vendor/autoload.php";
+require "connection.php";
+
+
+$app = new \atk4\ui\App("andrej");
+$app->initLayout("Centered");
+
+$model = new User($db);
+
+
+$form = $app->add(['Form']);
+$form->addField('nickname');
+$form->addField('password');
+$form->buttonSave->set("Войти");
+
+$form->onSubmit(function($form) use($model) {
+  $model->tryLoadBy('nickname',$form->model['nickname']);
+  if (isset($model->id)) {
+    if ($model["password"] == $form->model['password']) {
+
+    } else {
+      return new atk4\ui\jsNotify(['content' => 'Wrong input', 'color' => 'red']);
+    }
+
+  } else {
+    return new atk4\ui\jsNotify(['content' => 'Wrong input', 'color' => 'red']);
+  }
+
+});
