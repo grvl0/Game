b7524d1ac137b7790c64fc13bff793904dd534d3
gravl0 <andrej7aleksandrov@gmail.com>
2020-02-15 12:27:55 +0200

    New update

--- main.php
+++ main.php
@@ -12,13 +12,22 @@ $columns = $app->add('Columns');
 $col_1 = $columns->addColumn(3);
 $col_2 = $columns->addColumn(10);
 $col_3 = $columns->addColumn(3);
+
+$val=$col_2->add(['FormField/Line','-666']);
+
+
 $clikcer = $col_2->add(["Button",$_SESSION["i"],"green fluid big"]);
-$clikcer->on('click',function ($m) {
-    $_SESSION["i"] = $_SESSION["i"] + 1;
-    return $m;
-});
+$clikcer->js('click',new \atk4\ui\jsReload($val,['val' =>$val->jsInput()->val(new \atk4\ui\jsExpression('parseInt([])+10000000000', [$val->jsInput()->val()]), $val->jsInput()->focus())]));
+
 
 $save = $col_2->add(["Button","save","blue big"]);
 
 $exit = $app->add(['Button',"Exit","red"]);
 $exit->link(["exit"]);
+
+$x2 = $col_3->add(["Button","x2","inverted green"]);
+$pus = $col_3->add(["Button","не смеши мои тапки","inverted orange button"]);
+$cas = $col_3->add(["Button","mission pased","inverted blue"]);
+$gg = $col_3->add(["Button","aaaaaaaaa","inverted red"]);
+$jg = $col_3->add(["Button","x10000000","inverted yellow"]);
+$jg = $col_3->add(["Button","Я пыль","inverted primary"]);
